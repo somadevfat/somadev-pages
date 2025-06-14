fb8b0bc75de9d63d6601cf343ddb462438caac8f
soma-desktop-pc <somahiranodev@gmail.com>
2025-06-14 17:59:56 +0900

    ci: upload surefire reports artifact & test: use local profile for AuthIntegrationTest

--- backend/src/test/java/com/soma/backend/AuthIntegrationTest.java
+++ backend/src/test/java/com/soma/backend/AuthIntegrationTest.java
@@ -7,12 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("local")
 class AuthIntegrationTest {
 
     @Autowired
